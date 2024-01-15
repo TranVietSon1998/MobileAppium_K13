ce03fc2fd28c987d9ed92c8ca51a9dc28d02bb99
tvson <01263519039son@gmail.com>
2024-01-16 06:53:53 +0700

    Update lab2 base on Mr.Tu suggestion

--- src/main/java/labs/Lab2.java
+++ src/main/java/labs/Lab2.java
@@ -9,26 +9,31 @@ public class Lab2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Plz input tour height(m) and weight(kg)");
         System.out.print("Your height: ");
-        double height= scanner.nextDouble();
+        double height = scanner.nextDouble();
         System.out.print("Your weight: ");
-        double weight= scanner.nextDouble();
+        double weight = scanner.nextDouble();
         System.out.println();
 
-        String result = "BMI of 30 or greater. Plz decrease your weight ==> Bmi should be >18.5 && <24.9";
+
         double bmi = weight / (height * 2);
-        System.out.println("your bmi is: "+bmi);
+        double minNormalWeight = 18.5 * (height * 2);
 
-        if(bmi<=18.5) {
-            result = "Underweight. Plz increase your weight ==> Bmi should be >18.5 && <24.9";
-        }
-        else if(bmi <24.9) {
+        double maxNormalWeight = 24.9 * (height * 2);
+
+        String result = "BMI of 30 or greater. Plz decrease your weight ==> You should increase " + (weight - maxNormalWeight) + " kg";
+
+
+        System.out.println("your bmi is: " + bmi);
+
+        if (bmi <= 18.5) {
+            result = "Underweight. Plz increase your weight ==> You should increase " + (minNormalWeight - weight) + " kg";
+        } else if (bmi <= 24.9) {
             result = "Nornal weight. You're so handsome/beautiful";
-        }
-        else if(bmi<29.9) {
-            result = "Overweight.  Plz decrease your weight ==> Bmi should be >18.5 && <24.9";
+        } else if (bmi < 29.9) {
+            result = "Overweight.  Plz decrease your weight ==> You should decrease " + (weight - maxNormalWeight) + " kg";
         }
 
-        System.out.println("Your result is: "+ result);
+        System.out.println("Your result is: " + result);
         System.out.println("===============================");
 
 
@@ -36,10 +41,9 @@ public class Lab2 {
         System.out.println("Lab2.2");
         System.out.println("Plz input your number:");
         System.out.print("Your number is: ");
-        int yourNumber =scanner.nextInt();
-        String result1 = (yourNumber%2==0)? "even number" : "odd number";
-        System.out.println("Result is: "+result1);
-
+        int yourNumber = scanner.nextInt();
+        String result1 = (yourNumber % 2 == 0) ? "even number" : "odd number";
+        System.out.println("Result is: " + result1);
 
 
     }
