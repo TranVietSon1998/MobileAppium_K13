e24b9154e690ceb272806037714175da756a078e
tvson <01263519039son@gmail.com>
2024-02-28 20:36:44 +0700

    Update lab 7.2

--- src/main/java/labs/lab7_2/Animal.java
+++ src/main/java/labs/lab7_2/Animal.java
@@ -1,13 +1,26 @@
 package labs.lab7_2;
 
+import java.util.Random;
+
 public  class Animal {
+    private final String name;
+    private final int maxSpeed;
+
+    public Animal(String name, int maxSpeed) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+    }
+
     public int speed() {
-        // Default implementation, to be overridden by subclasses
-        return 0;
+        return new Random().nextInt(maxSpeed+1);
     }
 
     public String getName() {
-        // Default implementation, to be overridden by subclasses
-        return "";
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- src/main/java/labs/lab7_2/AnimalRace.java
+++ src/main/java/labs/lab7_2/AnimalRace.java
@@ -2,30 +2,25 @@ package labs.lab7_2;
 
 public class AnimalRace {
     public static void main(String[] args) {
-        Horse horse = new Horse();
-        Tiger tiger = new Tiger();
-        Dog dog = new Dog();
-
-        int horseSpeed = horse.speed();
-        int tigerSpeed = tiger.speed();
-        int dogSpeed = dog.speed();
-
-        String horseName = horse.getName();
-        String tigerName = tiger.getName();
-        String dogName = dog.getName();
-
-        System.out.println("Horse speed: " + horseSpeed + " km/h");
-        System.out.println("Tiger speed: " + tigerSpeed + " km/h");
-        System.out.println("Dog speed: " + dogSpeed + " km/h");
-
-        if (horseSpeed > tigerSpeed && horseSpeed > dogSpeed) {
-            System.out.println("Winner is: " + horseName + ", with speed is: " + horseSpeed);
-        } else if (tigerSpeed > horseSpeed && tigerSpeed > dogSpeed) {
-            System.out.println("Winner is: " + tigerName + ", with speed is: " + tigerSpeed);
-        } else if (dogSpeed > horseSpeed && dogSpeed > tigerSpeed) {
-            System.out.println("Winner is: " + dogName + ", with speed is: " + dogSpeed);
-        } else {
-            System.out.println("No animal wins");
-        }
+        Animal horse = new Animal("Horse", 75);
+        Animal tiger = new Animal("Tiger", 100);
+        Animal dog = new Animal("Dog", 60);
+        determineRaceWinner(horse, tiger, dog);
     }
+
+    public static void determineRaceWinner(Animal... animals) {
+        Animal winner = null;
+        int maxSpeed = -1;
+
+        for (Animal animal : animals) {
+            if (animal.speed() > maxSpeed) {
+                maxSpeed = animal.speed();
+                winner = animal;
+            }
+        }
+
+        System.out.println("Winner is " + winner + ", with speed: " + maxSpeed + " km/h");
+    }
+
+
 }
